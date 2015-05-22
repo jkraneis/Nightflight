760f7601688840ee3bce795f72187c3a47a301a9
Jan Kraneis <jkraneis@users.noreply.github.com>
2015-05-22 09:56:27 +0200

    Adopting KnightGoblin example

--- examples/KnightGoblin/KnightGoblin.ino
+++ examples/KnightGoblin/KnightGoblin.ino
@@ -1,77 +1,74 @@
- 
+  
+
 #include <FastLED.h>
 #include <SPI.h> // Included for SFE_LSM9DS0 library
-#include <Wire.h> // Included for SFE_LSM9DS0 library
+#include <i2c_t3.h> // Included for SFE_LSM9DS0 library
 #include <SFE_LSM9DS0.h>
 
 #include <Nightflight.h> //requires the FastLED library for LED functionality
 
+Parameter<uint8_t> rcChannelSteps(0);
+Parameter<uint8_t> rcChannelSteps2(0);
+
+//#include <Jan.h>
+#include <Ingbert.h>
+
+Parameter<uint8_t> brightnessParam(BRIGHTNESS);
 
 
-#define BRIGHTNESS  30
-#define CHIPSET     WS2811
-#define COLOR_ORDER GRB
+CRGB tronColor(24,202,230);
 
-#define PIN_HAUBE_RECHTS_OBEN  2
-#define PIN_HAUBE_LINKS_UNTEN 14
-#define PIN_HAUBE_LINKS_OBEN   7
-#define PIN_HAUBE_RECHTS_UNTEN  8
+Parameter<CRGB> riderColor(tronColor);
+Parameter<CRGB> riderColorWhite(CRGB::White);
+Parameter<CRGB> riderColorGreen(CRGB::Green);
+Parameter<CRGB> riderColorDarkBlue(CRGB::DarkBlue);
+Parameter<CRGB> riderColorBlack(CRGB::Black);
+Parameter<CRGB> riderResetColor(CRGB::Green);
+ColorReset riderReset(&riderResetColor);
+ColorReset riderResetBlack(&riderColorBlack);
+KnightRider rider11(0, 4, true, false, &riderColor, &riderReset);
+KnightRider rider12(0, 4, true, false, &riderColor, &riderReset);
+KnightRider rider13(0, 4, true, false, &riderColor, &riderReset);
+KnightRider rider14(0, 4, true, false, &riderColor, &riderReset);
 
-#define PIN_HECK_RECHTS_OBEN  6
-#define PIN_HECK_LINKS_UNTEN 20
-#define PIN_HECK_LINKS_OBEN   21
-#define PIN_HECK_RECHTS_UNTEN  5
-
-#define NUM_LEDS_HAUBE_LINKS_OBEN 39
-#define NUM_LEDS_HAUBE_LINKS_UNTEN 37
-#define NUM_LEDS_HECK_LINKS_OBEN 35
-#define NUM_LEDS_HECK_LINKS_UNTEN 46  //11 for the fin
-
-#define NUM_LEDS_HAUBE_RECHTS_OBEN 39
-#define NUM_LEDS_HAUBE_RECHTS_UNTEN 37
-#define NUM_LEDS_HECK_RECHTS_OBEN 35
-#define NUM_LEDS_HECK_RECHTS_UNTEN 46  //11 for the fin
-
-
-
-CRGB ledsHaubeLinksOben[NUM_LEDS_HAUBE_LINKS_OBEN];
-CRGB ledsHaubeLinksUnten[NUM_LEDS_HAUBE_LINKS_UNTEN];
-CRGB ledsHeckLinksOben[NUM_LEDS_HECK_LINKS_OBEN];
-CRGB ledsHeckLinksUnten[NUM_LEDS_HECK_LINKS_UNTEN];
-
-CRGB ledsHaubeRechtsOben[NUM_LEDS_HAUBE_RECHTS_OBEN];
-CRGB ledsHaubeRechtsUnten[NUM_LEDS_HAUBE_RECHTS_UNTEN];
-CRGB ledsHeckRechtsOben[NUM_LEDS_HECK_RECHTS_OBEN];
-CRGB ledsHeckRechtsUnten[NUM_LEDS_HECK_RECHTS_UNTEN];
-
-
-LEDStrip stripHaubeLinksOben(ledsHaubeLinksOben, NUM_LEDS_HAUBE_LINKS_OBEN, false);
-LEDStrip stripHaubeLinksUnten(ledsHaubeLinksUnten, NUM_LEDS_HAUBE_LINKS_UNTEN, false);
-LEDStrip stripHeckLinksOben(ledsHeckLinksOben, NUM_LEDS_HECK_LINKS_OBEN, true);
-LEDStrip stripHeckLinksUnten(ledsHeckLinksUnten, NUM_LEDS_HECK_LINKS_UNTEN, true, 0, 35);
-LEDStrip stripLinksFinne(ledsHeckLinksUnten, NUM_LEDS_HECK_LINKS_UNTEN, true, 35, 0);
-
-LEDStrip stripHaubeRechtsOben(ledsHaubeLinksOben, NUM_LEDS_HAUBE_RECHTS_OBEN, false);
-LEDStrip stripHaubeRechtsUnten(ledsHaubeLinksUnten, NUM_LEDS_HAUBE_RECHTS_UNTEN, false);
-LEDStrip stripHeckRechtsOben(ledsHeckLinksOben, NUM_LEDS_HECK_RECHTS_OBEN, true);
-LEDStrip stripHeckRechtsUnten(ledsHeckLinksUnten, NUM_LEDS_HECK_RECHTS_UNTEN, true, 0, 35);
-LEDStrip stripRechtsFinne(ledsHeckLinksUnten, NUM_LEDS_HECK_RECHTS_UNTEN, true, 35, 0);
-
-LEDVirtualStrip vStripLinksOben;
-LEDVirtualStrip vStripLinksUnten;
-LEDVirtualStrip vStripLinksFinne;
-
-LEDVirtualStrip vStripRechtsOben;
-LEDVirtualStrip vStripRechtsUnten;
-LEDVirtualStrip vStripRechtsFinne;
+Fire fire1(70, 120);
+Fire fire2(80, 120);
+Fire fire3(90, 120);
+Fire fire4(80, 120);
 
 
 Parameter<CRGB> lineColor(CRGB::Red);
 
 Line line1(&lineColor);
 
-Parameter<uint8_t> brightnessParam(BRIGHTNESS);
-Parameter<uint8_t> rcChannelSteps(0);
+Parameter<uint8_t> position(40);
+Parameter<uint8_t> radius(0);
+ParameterTimeLinear radiusLinearRadius(0, 40, 2000, &radius, 1);
+Radius radiusLinear(&position, &radius, &riderColorDarkBlue, false);
+ParameterTimeLinear radiusLinearRadiusDown(40, 0, 300, &radius, 0);
+
+Parameter<uint8_t> radius2(0);
+ParameterTimeLinear radiusLinearRadius2(0, 36, 4000, &radius2, 2);
+Radius radiusLinear2(&position, &radius2, &riderColorGreen, true);
+ParameterTimeLinear radiusLinearRadius2Down(36, 0, 300, &radius2, 0);
+
+Parameter<uint8_t> radius3(0);
+ParameterTimeLinear radiusLinearRadius3(0, 32, 6629, &radius3, 3);
+Radius radiusLinear3(&position, &radius3, &riderColorWhite, true);
+ParameterTimeLinear radiusLinearRadius3Down(32, 0, 300, &radius3, 0);
+
+//YAW thing
+Parameter<uint8_t> positionYaw(30);
+Parameter<uint8_t> positionYaw2(60);
+Parameter<uint8_t> positionYaw3(90);
+Parameter<uint8_t> positionYaw4(120);
+Parameter<uint8_t> radiusYawWidth(15);
+Radius radiusYaw(&positionYaw, &radiusYawWidth, &riderColorDarkBlue, false, true);
+Radius radiusYaw2(&positionYaw2, &radiusYawWidth, &riderColorGreen, false, true);
+Radius radiusYaw3(&positionYaw3, &radiusYawWidth, &lineColor, false, true);
+Radius radiusYaw4(&positionYaw4, &radiusYawWidth, &riderColorWhite, false, true);
+
+
 
 
 //This has to be created in the sketch since including libraries in libraries does not work with the Arduino ide...
@@ -83,48 +80,22 @@ void setup() {
 
   Nightflight.setMainLoopCallback(renderTimerInfoMainLoopCallback);
   Nightflight.setBPMLoopCallback(bpmTimerInfoLoopCallback);
-  Nightflight.setBPM(120.0);
+  Nightflight.setBPM(123.0);
   
   Nightflight.setLSM9DS0(&dof);
-  dof.setAccelScale(dof.A_SCALE_8G);
-  dof.setAccelODR(dof.A_ODR_100);
-  dof.setGyroScale(dof.G_SCALE_2000DPS);
-  dof.setGyroODR(dof.G_ODR_380_BW_50);
-  dof.setMagScale(dof.M_SCALE_8GS);
-  dof.setMagODR(dof.M_ODR_50);
   
   
-  //set up the LED hardware
-  FastLED.addLeds<CHIPSET, PIN_HAUBE_LINKS_OBEN, COLOR_ORDER>(ledsHaubeLinksOben, NUM_LEDS_HAUBE_LINKS_OBEN).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, PIN_HAUBE_LINKS_UNTEN, COLOR_ORDER>(ledsHaubeLinksUnten, NUM_LEDS_HAUBE_LINKS_UNTEN).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, PIN_HECK_LINKS_OBEN, COLOR_ORDER>(ledsHeckLinksOben, NUM_LEDS_HECK_LINKS_OBEN).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, PIN_HECK_LINKS_UNTEN, COLOR_ORDER>(ledsHeckLinksUnten, NUM_LEDS_HECK_LINKS_UNTEN).setCorrection( TypicalLEDStrip );
-  
-  FastLED.addLeds<CHIPSET, PIN_HAUBE_RECHTS_OBEN, COLOR_ORDER>(ledsHaubeRechtsOben, NUM_LEDS_HAUBE_RECHTS_OBEN).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, PIN_HAUBE_RECHTS_UNTEN, COLOR_ORDER>(ledsHaubeRechtsUnten, NUM_LEDS_HAUBE_RECHTS_UNTEN).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, PIN_HECK_RECHTS_OBEN, COLOR_ORDER>(ledsHeckRechtsOben, NUM_LEDS_HECK_RECHTS_OBEN).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<CHIPSET, PIN_HECK_RECHTS_UNTEN, COLOR_ORDER>(ledsHeckRechtsUnten, NUM_LEDS_HECK_RECHTS_UNTEN).setCorrection( TypicalLEDStrip );
-  
-  vStripLinksOben.addLEDStrip(stripHaubeLinksOben);
-  vStripLinksOben.addLEDStrip(stripHeckLinksOben);  
-  vStripLinksFinne.addLEDStrip(stripLinksFinne);
-  vStripLinksUnten.addLEDStrip(stripHaubeLinksUnten);
-  vStripLinksUnten.addLEDStrip(stripHeckLinksUnten);
-
-  vStripRechtsOben.addLEDStrip(stripHaubeRechtsOben);
-  vStripRechtsOben.addLEDStrip(stripHeckRechtsOben);  
-  vStripRechtsFinne.addLEDStrip(stripRechtsFinne);
-  vStripRechtsUnten.addLEDStrip(stripHaubeRechtsUnten);
-  vStripRechtsUnten.addLEDStrip(stripHeckRechtsUnten);
-
- 
+  setupIndividual();
   FastLED.setBrightness( BRIGHTNESS );
  
-  Nightflight.startBPMTimer();
+  radiusLinear.setBackgroundEffect(&riderResetBlack);
+  radiusLinear2.setBackgroundEffect(&radiusLinear);
+  radiusLinear3.setBackgroundEffect(&radiusLinear2);
+ 
+  radiusYaw.setBackgroundEffect(&riderResetBlack);
 }
 
 
-//boolean to check if a FastLED Render was performed
 
 void loop()
 {
@@ -144,95 +115,180 @@ uint8_t rColorHue = 0;
 void bpmTimerInfoLoopCallback()
 {
   beats++;
+ // if(Nightflight.getBeatIsDownbeat())
+  {
+    Serial.print("Beats: ");
+    Serial.println(beats);
+    if(beats == 21)
+    {
+      radiusLinearRadius.start();
+      radiusLinearRadius2.start();
+      radiusLinearRadius3.start();
+    }
+    if(beats == 44)
+    {
+      radiusLinearRadius.stop();
+      radiusLinearRadius2.stop();
+      radiusLinearRadius3.stop();
+      radiusLinearRadiusDown.start();
+      radiusLinearRadius2Down.start();
+      radiusLinearRadius3Down.start();
+    }
+  }
+  if(!bpmRender)
+  {
+    return;
+  }
+  if(Nightflight.getBeatIsDownbeat())
+  {
+    //Serial.print(" isDownbeat ");
+    FastLED.setBrightness( 255 );
+  }
+  else
+  {
+    FastLED.setBrightness( 50 );
+  }
+  FastLED.show();
+  bpmRenderNext = true;
   
+}
+
+
+boolean bpmTimerStarted = false;
+void startBPMTimer()
+{
+  if(!bpmTimerStarted)
+  {
+    Nightflight.startBPMTimer();
+    bpmTimerStarted = true;
+  }
 }
 
 void waitStartSequenceRender()
 {
-  line1.setEnd(2);
-  line1.render(stripHaubeLinksOben, 0);
-  line1.setEnd(1);
-  line1.render(stripHaubeLinksUnten, 0);
-  line1.setEnd(6);
-  line1.render(stripHeckLinksOben, 0);
-  line1.setEnd(5);
-  line1.render(stripHeckLinksUnten, 0);
-
-  line1.setEnd(0);
-  line1.render(stripHaubeRechtsOben, 0);
-  line1.setEnd(3);
-  line1.render(stripHaubeRechtsUnten, 0);
-  line1.setEnd(4);
-  line1.render(stripHeckRechtsOben, 0);
-  line1.setEnd(7);
-  line1.render(stripHeckRechtsUnten, 0);
-    
-    
+    rider11.render(vStripLinksOben, 0);
+    rider12.render(vStripLinksUnten, 0);
+    rider13.render(vStripRechtsOben, 0);
+    rider14.render(vStripRechtsUnten, 0);
 }
 
-uint8_t brightnessByAccelfade = 20;
+void fire()
+{
+    fire1.render(vStripLinksOben, 0);
+    fire2.render(vStripLinksUnten, 0);
+    fire3.render(vStripRechtsOben, 0);
+    fire4.render(vStripRechtsUnten, 0);
+}
+
+void THX()
+{
+    radiusLinearRadius.render();
+    radiusLinearRadius2.render();
+    radiusLinearRadius3.render();
+    radiusLinearRadiusDown.render();
+    radiusLinearRadius2Down.render();
+    radiusLinearRadius3Down.render();
+    radiusLinear3.render(vStripRechtsOben,0);
+    radiusLinear3.render(vStripRechtsUnten,0);
+    radiusLinear3.render(vStripLinksOben,0);
+    radiusLinear3.render(vStripLinksUnten,0);
+    brightnessParam.setValue(radius3.getValue()*7);
+}
+
+void Yaw()
+{
+    uint8_t yaw = (uint8_t)fmap(Nightflight.yaw, -180.0, 180.0, 0.0, 152.0);
+    positionYaw.setValue(yaw);
+    positionYaw2.setValue((yaw + 37) % 152);
+    positionYaw3.setValue((yaw + 74) % 152);
+    positionYaw4.setValue((yaw + 111) % 152);
+    radiusYaw.render(vStripObenRundrum, 0);
+    radiusYaw2.render(vStripObenRundrum, 0);
+    radiusYaw3.render(vStripObenRundrum, 0);
+    radiusYaw4.render(vStripObenRundrum, 0);
+    radiusYaw.render(vStripUntenRundrum, 0);
+    radiusYaw2.render(vStripUntenRundrum, 0);
+    radiusYaw3.render(vStripUntenRundrum, 0);
+    radiusYaw4.render(vStripUntenRundrum, 0);
+}
+
+
+uint8_t brightnessByAccelfade = 11;
 
 void brightnessByAccel()
 {
     uint8_t currentBrightness = brightnessParam.getValue();
     uint8_t absAccel = (uint8_t)fmap(Nightflight.getAbsoluteAcceleration(), 0.0, 16.0, 0.0, 255.0);
-    if(absAccel > currentBrightness)
-    {
-      brightnessParam.setValue(absAccel);
-    }
-    else
-    {
-      brightnessParam.setValue(currentBrightness >= brightnessByAccelfade ? currentBrightness - brightnessByAccelfade : 0);
-    }
+
+    brightnessParam.setValue(absAccel);
 }
 
 void renderTimerInfoMainLoopCallback()
 {
   //first evaluate the input from the RC Channel, currently splitting to 6 steps
-  rcChannelSteps.setValue(map(Nightflight.currentChannelData, 1120, 1920, 0, 5));
+  setRcChannelValues();
   //then switch programs according to RC Channel value
   uint32_t theValue = rcChannelSteps.getValue();
+ // uint32_t theValue2 = rcChannelSteps2.getValue();
+/*  Serial.print("RC PWM1 value: ");
+  Serial.println(Nightflight.currentChannelData);
+  Serial.print("RC PWM2 value: ");
+  Serial.println(Nightflight.currentChannelData2);
   Serial.print("RC LED Program Selection: ");
-  Serial.println(theValue);
+  Serial.println(theValue);*/
   
   //if( !bpmRender || (bpmRender && bpmRenderNext) )
   {
     switch(theValue)
     {
-      case 5:  break;
-      case 4:  break;
-      case 3:  break;
-      case 2:   bpmRender = true;
+      case 7:   
+      case 8:    bpmRender = false;
+                bpmRenderNext = false;    
+                brightnessByAccel();
+                fire();
+                break;
+
+      case 9:   bpmRender = true;
+                bpmRenderNext = false;
+                brightnessByAccel();
+                riderColor.setValue(CRGB::Red);
+                riderResetColor.setValue(tronColor);
+                waitStartSequenceRender();
+                break;
+      case 6:   bpmRender = false;
                 bpmRenderNext = false;    
                 brightnessParam.setValue(BRIGHTNESS);
+                fire();
+                break;
+      case 5:
+      case 4:   bpmRender = true;
+                bpmRenderNext = false;    
+                //brightnessByAccel();
+                riderColor.setValue(CRGB::Red);
+                riderResetColor.setValue(tronColor);
                 waitStartSequenceRender();
                 break;
-      case 1:   bpmRender = false;
-                brightnessByAccel();
-                waitStartSequenceRender();
-                break;
-      case 0:   bpmRender = false;
+      case 3:   bpmRender = true;
+                bpmRenderNext = false;    
+                riderColor.setValue(tronColor);
+                riderResetColor.setValue(CRGB::Green);
+               // brightnessByAccel();
                 brightnessParam.setValue(BRIGHTNESS);
                 waitStartSequenceRender();
+                break;
+      case 2:  bpmRender = true;
+                bpmRenderNext = false;
+                brightnessParam.setValue(BRIGHTNESS);
+                Yaw();
+                break;
+      case 1:   THX();
+                break;
+      case 0:   startBPMTimer();
                 break;
       default:  break;
      }
    }
 
-
-    //fire2.render(vStripLinksUnten, 0);
-    //rainbow2.render(vStripHeckFinne, 0);
-    /*
-    line1.setStart((uint8_t)((Nightflight.currentChannelData-1120)/30) % 30);
-    line1.setEnd((uint8_t)((Nightflight.currentChannelData-1120)/30 + 3) % 30);
-    line1.render(vStripHeckFinne, 0);*/
-    
-    //  fade1.render(vStripHeckFinne, 0);
-    //br1.render(vStripHeckFinne, 0);
-    /*if(fade1.fadeFinished())
-    {
-      fade1.reset();
-    }*/
     FastLED.setBrightness( brightnessParam.getValue() );
 }
 
